0288425d7cf3d450b958d133d7dc2b3a88a4e348
Bruno Padua <bruno.paiva@forlogic.net>
2017-11-20 13:24:20 -0200

    feat: estrutura de salas

---
+++ src/app/rooms/rooms.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomListComponent } from './room-list/room-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RoomListComponent]
+})
+export class RoomsModule { }
