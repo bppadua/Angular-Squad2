e6878670bf803bc107cc4039905c607180d24632
Bruno Padua <bruno.paiva@forlogic.net>
2017-11-27 14:14:50 -0200

    utilizando o flex layout na sidenav

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,6 +1,9 @@
+import { User } from './../../users/shared/user.model';
 import { Component, OnInit } from '@angular/core';
 import { Room } from './../shared/room.model';
 import { MaterialModule } from './../../shared/material/material.module';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
@@ -9,15 +12,21 @@ import { MaterialModule } from './../../shared/material/material.module';
 export class RoomListComponent implements OnInit {
 
   public rooms : Room[] = new Array<Room>(); 
-  private room : Room = new Room();
-  constructor() { }
-
+  mqAlias: string;
+  constructor(    private media: ObservableMedia,
+  ) { }
+  
   ngOnInit() {
-    for(var i = 1; i< 10; i++){
-      this.room.identifier=i+'';
-      this.room.desc='sadasda';
-      this.room.name='Sala '+i; 
-      this.rooms.push(this.room);
+    this.mqAlias = this.media.isActive('xs') || this.media.isActive('sm') ? "sm" : "md";
+    this.media.subscribe((change: MediaChange) => {
+      this.mqAlias = change.mqAlias;
+    });
+    for(var i = 1; i < 10; i++){
+      let teste = new Room()
+      teste.identifier=i+'';
+      teste.desc='sadasda';
+      teste.name='Sala '+i; 
+      this.rooms.push(teste);
     }
   }
 
