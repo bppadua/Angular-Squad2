e6a56fec8da0651e7e1ec5cd52039647e932bada
Bruno Padua <bruno.paiva@forlogic.net>
2017-11-27 10:48:24 -0200

    room list

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Room } from './../shared/room.model';
+import { MaterialModule } from './../../shared/material/material.module';
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomListComponent implements OnInit {
 
+  private rooms : Room[] = new Array<Room>(); 
+  private room : Room = new Room();
   constructor() { }
 
   ngOnInit() {
+    for(var i = 1; i< 10; i++){
+      this.room.identifier=i+'';
+      this.room.desc='sadasda';
+      this.room.name='Sala '+i; 
+      this.rooms.push(this.room);
+    }
   }
 
 }
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -9,7 +9,8 @@ import {
 	MatInputModule,
 	MatCardModule,
 	MatSelectModule,
-	MatTabsModule
+	MatTabsModule,
+	MatListModule
 } from '@angular/material';
 
 @NgModule({
@@ -22,7 +23,8 @@ import {
 		MatInputModule,
 		MatCardModule,
 		MatSelectModule,
-		MatTabsModule
+		MatTabsModule,
+		MatListModule
 	],
 	exports: [
 		CommonModule,
@@ -33,7 +35,8 @@ import {
 		MatInputModule,
 		MatCardModule,
 		MatSelectModule,
-		MatTabsModule
+		MatTabsModule,
+		MatListModule
 	]
 })
 export class MaterialModule { }
