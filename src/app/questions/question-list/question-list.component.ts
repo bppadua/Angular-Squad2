32824816ea8d41c7eed3a5d3e767bc303fd3cfee
Alysson Lopes Cordeiro <alysson@FORLOGIC.LOCAL>
2017-12-04 13:26:17 -0200

    feat: Criar pergunta

--- src/app/questions/question-list/question-list.component.ts
+++ src/app/questions/question-list/question-list.component.ts
@@ -1,3 +1,4 @@
+import { QuestionsCrudComponent } from './../questions-crud/questions-crud.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {MaterialModule} from '../../shared/material/material.module';
@@ -7,10 +8,16 @@ import {MaterialModule} from '../../shared/material/material.module';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
-
+  public question: QuestionsCrudComponent = new QuestionsCrudComponent();
   constructor() { }
 
   ngOnInit() {
   }
+
+  debug(): string {
+    let arr = JSON.parse(localStorage.getItem("questions"));
+    //localStorage.setItem('questions', JSON.stringify(arr))
+		return JSON.stringify(arr);
+	}
 
 }
--- src/app/questions/shared/question.model.ts
+++ src/app/questions/shared/question.model.ts
@@ -5,6 +5,8 @@ export class Question {
     constructor(
         public identifier?: string,
         public question?: string,
+        public name?: string,
+        public desc?: string,
         public room?: Room,
         public isActive?: boolean
       ) { }
