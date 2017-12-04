4cd87ac5f690f7d1d52291528c08dfdf720fef96
andrersilva10 <andre.002016@gmail.com>
2017-12-04 09:43:14 -0200

    feat: criação da sala em sidenav

--- src/app/questions/question-list/question-list.component.ts
+++ src/app/questions/question-list/question-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {MaterialModule} from '../../shared/material/material.module';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
